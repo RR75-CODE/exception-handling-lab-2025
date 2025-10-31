581d74c5e45c88bdcfcfc297561cb6b17f2dfca0
RR75-CODE <ryanradieddine@gmail.com>
2025-10-30 23:31:04 -0400

    Add files via upload

---
+++ Lab5-part1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace Lab5_Part1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.Write("Enter first number: ");
+                double num1 = Convert.ToDouble(Console.ReadLine());
+
+                Console.Write("Enter second number: ");
+                double num2 = Convert.ToDouble(Console.ReadLine());
+
+                double result = num1 / num2;
+                Console.WriteLine($"Result: {result}");
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Enter only numbers.");
+            }
+            catch (DivideByZeroException)
+            {
+                Console.WriteLine("You cannot divide by zero.");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("An error occurred:" + ex.Message);
+            }
+            finally
+            {
+                using (EventLog eventLog = new EventLog("Application"))
+                {
+                    eventLog.Source = "Application";
+                    eventLog.WriteEntry("Log message example", EventLogEntryType.Information, 101, 1);
+                }
+
+                Console.WriteLine("Event written to Application Log.");
+            }
+        }
+    }
+}
+
---
+++ Lab5-part2.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+public class PasswordComplexityException : Exception
+{
+    public PasswordComplexityException(string message) : base(message)
+    {
+    }
+}
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Enter your password: ");
+        string password = Console.ReadLine();
+
+        try
+        {
+            CheckPassword(password);
+            Console.WriteLine("Password accepted.");
+        }
+        catch (PasswordComplexityException ex)
+        {
+            Console.WriteLine("Password Error: " + ex.Message);
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("Unexpected error: " + ex.Message);
+        }
+    }
+    static void CheckPassword(string password)
+    {
+        if (password.Length < 8)
+        {
+            throw new PasswordComplexityException("Password must be at least 8 characters long.");
+        }
+    }
+}
+
